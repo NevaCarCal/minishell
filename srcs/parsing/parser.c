ec9fe61732293c0fea1e5af10e813a571dd362d0
Neva <neva@neva.im>
2026-02-10 22:34:07 +0100

    Fixed scenarios where minishell would have a status of 0 when invalid pipes and invalid commands were passed, where bash would be status 2

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: ncarrera <ncarrera@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 01:55:00 by antigravity       #+#    #+#             */
-/*   Updated: 2025/11/26 13:16:25 by ncarrera         ###   ########.fr       */
+/*   Updated: 2026/02/10 22:31:42 by ncarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,45 @@ static t_redir_type	get_redir_type(char *token)
 	return (REDIR_IN);
 }
 
-static int	handle_token(t_command **curr, char *token, t_parse_info *info)
+static int	redir_token(t_command **curr, char *token, t_parse_info *info)
 {
 	char			*next_tok;
 	t_redir_type	type;
 
+	type = get_redir_type(token);
+	next_tok = get_next_token(info->line, info->i);
+	if (!next_tok)
+	{
+		info->shell->exit_code = 2;
+		return (0);
+	}
+	if (is_operator(next_tok[0]))
+	{
+		info->shell->exit_code = 2;
+		free(next_tok);
+		return (0);
+	}
+	add_redirection(*curr, type, next_tok, info->shell);
+	free(next_tok);
+	return (1);
+}
+
+static int	handle_token(t_command **curr, char *token, t_parse_info *info)
+{
 	if (ft_strncmp(token, "|", 2) == 0)
 	{
+		if (!(*curr)->args && !(*curr)->redirs)
+		{
+			info->shell->exit_code = 2;
+			return (0);
+		}
 		(*curr)->next = new_command();
 		*curr = (*curr)->next;
 	}
 	else if (is_operator(token[0]))
 	{
-		type = get_redir_type(token);
-		next_tok = get_next_token(info->line, info->i);
-		if (!next_tok)
+		if (!redir_token(curr, token, info))
 			return (0);
-		add_redirection(*curr, type, next_tok, info->shell);
-		free(next_tok);
 	}
 	else
 		add_argument(*curr, token, info->shell);
